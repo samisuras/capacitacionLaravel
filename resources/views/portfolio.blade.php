13aa948b1a0cff6de6afc6af1e638d3ea6f4add6
samisuras <raulsamuelleos98@gmail.com>
2019-09-20 14:10:40 -0500

    Estructuras de control con Blade

--- resources/views/portfolio.blade.php
+++ resources/views/portfolio.blade.php
@@ -2,4 +2,13 @@
 @section('title','Portfolio')
 @section('content')
     <h1>Portfolio</h1>
+
+    <ul>
+    </ul>
+        @forelse($portfolio as $portfolioItem)
+            <li>{{$portfolioItem['title']}} <pre>{{ $loop->last ? 'es el ultimo' : '' }}</pre> </li>
+            @empty
+            <li>No hay proyectos que mostrar</li>
+        @endforelse
+    </ul>
 @endsection
