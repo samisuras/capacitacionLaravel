b8d1a5bc37ef3126034164cec18393c35d68616f
samisuras <raulsamuelleos98@gmail.com>
2019-09-20 14:26:06 -0500

    Controladores

--- routes/web.php
+++ routes/web.php
@@ -10,18 +10,11 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-$portfolio = [
-    ['title' => 'Proyecto #1'],
-    ['title' => 'Proyecto #2'],
-    ['title' => 'Proyecto #3'],
-    ['title' => 'Proyecto #4']
-];
 
 Route::view('/','home')->name('home');
 Route::view('/about','about')->name('about');
-Route::view('/portfolio','portfolio', compact('portfolio'))->name('portfolio');
 Route::view('/contact','contact')->name('contact');
-
+Route::get('/portfolio','PortfolioController')->name('portfolio');
 //Route::view('/','home');
 
 //Route::get('/', function () {
