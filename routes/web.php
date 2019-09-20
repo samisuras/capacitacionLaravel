09ed285abbb92e572bc77ef906062082d045932a
samisuras <raulsamuelleos98@gmail.com>
2019-09-20 14:50:31 -0500

    Controladores Api y Resource

--- routes/web.php
+++ routes/web.php
@@ -11,15 +11,10 @@
 |
 */
 
-Route::view('/','home')->name('home');
-Route::view('/about','about')->name('about');
-Route::view('/contact','contact')->name('contact');
-Route::get('/portfolio','PortfolioController')->name('portfolio');
-//Route::view('/','home');
+//Route::view('/','home')->name('home');
+//Route::view('/about','about')->name('about');
+//Route::view('/contact','contact')->name('contact');
+//Route::get('/portfolio','PortfolioController@index');
 
-//Route::get('/', function () {
-//    $nombre = "Sam";
-//    return view('home', compact('nombre'));
-//})->name('home');
-//
+Route::apiResource('projects','PortfolioController');
 
